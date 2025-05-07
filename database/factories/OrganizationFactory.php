d4c632a05acfe74bfc146f46037ff2d975d1e29f
MoisesCorcho <mcorchoperez@gmail.com>
2025-05-07 13:29:41 -0500

    feat(Factory): User and Orgnizations factories have been added

--- database/factories/OrganizationFactory.php
+++ database/factories/OrganizationFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +18,66 @@ class OrganizationFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'id' => $this->faker->uuid(),
+            'name' => $this->faker->company(),
+            'description' => $this->faker->paragraph(),
+            'nit' => $this->faker->numerify('###-###-###'),
+            'cellphone' => $this->faker->numerify('##########'),
+            'phone' => $this->faker->numerify('########'),
+            'email' => $this->faker->unique()->companyEmail(),
+            'user_id' => User::factory(),
         ];
+    }
+
+    /**
+     * Configure the organization with an existing user.
+     */
+    public function forUser(User $user): static
+    {
+        return $this->state(fn(array $attributes) => [
+            'user_id' => $user->id,
+        ]);
+    }
+
+    /**
+     * Configure the organization with a specific name.
+     */
+    public function withName(string $name): static
+    {
+        return $this->state(fn(array $attributes) => [
+            'name' => $name,
+        ]);
+    }
+
+    /**
+     * Configure the organization with a specific description.
+     */
+    public function withDescription(string $description): static
+    {
+        return $this->state(fn(array $attributes) => [
+            'description' => $description,
+        ]);
+    }
+
+    /**
+     * Configure the organization with a specific NIT.
+     */
+    public function withNit(string $nit): static
+    {
+        return $this->state(fn(array $attributes) => [
+            'nit' => $nit,
+        ]);
+    }
+
+    /**
+     * Configure the organization with specific contact information.
+     */
+    public function withContactInfo(string $email, string $cellphone, ?string $phone = null): static
+    {
+        return $this->state(fn(array $attributes) => [
+            'email' => $email,
+            'cellphone' => $cellphone,
+            'phone' => $phone ?? $this->faker->numerify('########'),
+        ]);
     }
 }
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Enums\DniType;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
@@ -24,11 +25,17 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'name' => $this->faker->firstName(),
+            'last_name' => $this->faker->lastName(),
+            'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'password' => Hash::make('password'),
+            'remember_token' => null,
+            'cellphone' => $this->faker->numerify('##########'),
+            'dni_type' => $this->faker->randomElement(DniType::cases())->value,
+            'dni' => $this->faker->numerify('########'),
+            'active' => true,
+            'visits_per_day' => $this->faker->numberBetween(1, 10),
         ];
     }
 
@@ -37,8 +44,39 @@ class UserFactory extends Factory
      */
     public function unverified(): static
     {
-        return $this->state(fn (array $attributes) => [
+        return $this->state(fn(array $attributes) => [
             'email_verified_at' => null,
+        ]);
+    }
+
+    /**
+     * Indicate that the user should be inactive.
+     */
+    public function inactive(): static
+    {
+        return $this->state(fn(array $attributes) => [
+            'active' => false,
+        ]);
+    }
+
+    /**
+     * Indicate that the user should have a specific number of visits per day.
+     */
+    public function withVisitsPerDay(int $visits): static
+    {
+        return $this->state(fn(array $attributes) => [
+            'visits_per_day' => $visits,
+        ]);
+    }
+
+    /**
+     * Indicate that the user should have a specific type of identification.
+     */
+    public function withIdentification(string $type, string $number): static
+    {
+        return $this->state(fn(array $attributes) => [
+            'dni_type' => $type,
+            'dni' => $number,
         ]);
     }
 }
