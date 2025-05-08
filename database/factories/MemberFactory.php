62d2e78e7b8df386371fe74a77d7b92db5cc0dea
MoisesCorcho <mcorchoperez@gmail.com>
2025-05-07 22:49:30 -0500

    feat: Add fields to MemberFactory and create MemberPositionFactory

--- database/factories/MemberFactory.php
+++ database/factories/MemberFactory.php
@@ -2,6 +2,9 @@
 
 namespace Database\Factories;
 
+use App\Enums\DniType;
+use App\Models\Organization;
+use App\Models\MemberPosition;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +20,17 @@ class MemberFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'first_name' => $this->faker->firstName(),
+            'last_name' => $this->faker->lastName(),
+            'dni_type' => $this->faker->randomElement(DniType::cases())->value,
+            'dni' => $this->faker->regexify('[0-9]{8}'),
+            'cellphone_1' => $this->faker->numerify('##########'),
+            'cellphone_2' => $this->faker->unique()->numerify('##########'),
+            'phone' => $this->faker->numerify('+1##########'), // Includes a country code (e.g., +1 for the US)
+            'birthdate' => $this->faker->date(),
+            'email' => $this->faker->safeEmail(),
+            'organization_id' => Organization::factory(),
+            'member_position_id' => MemberPosition::factory(),
         ];
     }
 }
