b7849b2740091d05f655fff91310d94926beafe8
MoisesCorcho <mcorchoperez@gmail.com>
2025-05-08 18:10:02 -0500

    feat: Update VisitFactory to include visit status logic

--- database/factories/VisitFactory.php
+++ database/factories/VisitFactory.php
@@ -2,6 +2,10 @@
 
 namespace Database\Factories;
 
+use App\Enums\VisitStatusEnum;
+use App\Models\User;
+use App\Models\Organization;
+use App\Models\NoVisitReason;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +21,67 @@ class VisitFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'visit_date' => $this->faker->dateTimeBetween('-1 week', '+1 week'),
+            'rescheduled_date' => $this->faker->dateTimeBetween('-1 week', '+1 week'),
+            'non_visit_reason' => $this->faker->sentence(),
+            'status' => $this->faker->randomElement(VisitStatusEnum::cases())->value,
+            'organization_id' => Organization::factory(),
+            'user_id' => User::factory(),
+            'non_visit_reason_id' => NoVisitReason::factory(),
         ];
+    }
+
+    public function visited(): static
+    {
+        return $this->state(fn(array $attributes) => [
+            'visit_date' => $this->faker->dateTimeBetween('-10 day', '-1 day'),
+            'rescheduled_date' => null,
+            'status' => VisitStatusEnum::VISITED->value,
+            'non_visit_reason' => null,
+            'non_visit_reason_id' => null,
+        ]);
+    }
+
+    public function notVisited(): static
+    {
+        return $this->state(fn(array $attributes) => [
+            'visit_date' => $this->faker->dateTimeBetween('-10 day', '-1 day'),
+            'rescheduled_date' => null,
+            'status' => VisitStatusEnum::NOT_VISITED->value,
+            'non_visit_reason_id' => NoVisitReason::factory(),
+        ]);
+    }
+
+    public function canceled(): static
+    {
+        return $this->state(fn(array $attributes) => [
+            'visit_date' => $this->faker->dateTimeBetween('-2 day', '+10 day'),
+            'rescheduled_date' => null,
+            'status' => VisitStatusEnum::CANCELED->value,
+            'non_visit_reason' => $this->faker->sentence(),
+            'non_visit_reason_id' => NoVisitReason::factory(),
+        ]);
+    }
+
+    public function scheduled(): static
+    {
+        return $this->state(fn(array $attributes) => [
+            'visit_date' => $this->faker->dateTimeBetween('+2 day', '+10 day'),
+            'rescheduled_date' => null,
+            'status' => VisitStatusEnum::SCHEDULED->value,
+            'non_visit_reason' => null,
+            'non_visit_reason_id' => null,
+        ]);
+    }
+
+    public function rescheduled(): static
+    {
+        return $this->state(fn(array $attributes) => [
+            'visit_date' => $this->faker->dateTimeBetween('+2 day', '+10 day'),
+            'rescheduled_date' => $this->faker->dateTimeBetween('+2 day', '+10 day'),
+            'status' => VisitStatusEnum::RESCHEDULED->value,
+            'non_visit_reason' => null,
+            'non_visit_reason_id' => null,
+        ]);
     }
 }
