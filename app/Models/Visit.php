8741722f843c977290bc00364fdb508227411ad8
MoisesCorcho <mcorchoperez@gmail.com>
2025-05-08 16:58:07 -0500

    feat: Add Visit model and migration with fillable fields and relationships

--- app/Models/Visit.php
+++ app/Models/Visit.php
@@ -4,9 +4,29 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Visit extends Model
 {
     /** @use HasFactory<\Database\Factories\VisitFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'visit_date',
+        'rescheduled_date',
+        'non_visit_reason',
+        'status',
+        'organization_id',
+        'user_id',
+    ];
+
+    public function organization(): BelongsTo
+    {
+        return $this->belongsTo(Organization::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- database/migrations/2025_05_08_204536_create_visits_table.php
+++ database/migrations/2025_05_08_204536_create_visits_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Enums\VisitStatusEnum;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +14,12 @@ return new class extends Migration
     {
         Schema::create('visits', function (Blueprint $table) {
             $table->id();
+            $table->date('visit_date');
+            $table->date('rescheduled_date')->nullable();
+            $table->text('non_visit_reason')->nullable();
+            $table->string('status')->default(VisitStatusEnum::SCHEDULED->value);
+            $table->foreignUuid('organization_id')->constrained();
+            $table->foreignId('user_id')->constrained();
             $table->timestamps();
         });
     }
