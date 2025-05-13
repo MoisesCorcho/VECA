103dccc705fd88de610cbb1338bc28d384e6d51a
MoisesCorcho <mcorchoperez@gmail.com>
2025-05-13 11:46:44 -0500

    feat: Add label, keyValuesCombined, and nonOptionsTypes methods to SurveyQuestionsTypeEnum

--- app/Enums/SurveyQuestionsTypeEnum.php
+++ app/Enums/SurveyQuestionsTypeEnum.php
@@ -17,4 +17,33 @@ enum SurveyQuestionsTypeEnum: string
     {
         return collect(self::cases())->pluck('value');
     }
+
+    public function label(): string
+    {
+        return match ($this) {
+            self::TYPE_TEXT     => __('Text'),
+            self::TYPE_TEXTAREA => __('Textarea'),
+            self::TYPE_SELECT   => __('Select'),
+            self::TYPE_RADIO    => __('Radio'),
+            self::TYPE_CHECKBOX => __('Checkbox'),
+            self::TYPE_DATE     => __('Date'),
+        };
+    }
+
+    public static function keyValuesCombined(): array
+    {
+        return array_combine(
+            array_column(self::cases(), 'value'),
+            array_map(fn($case) => $case->label(), self::cases())
+        );
+    }
+
+    public static function nonOptionsTypes(): array
+    {
+        return collect(self::cases())
+            ->pluck('value')
+            ->filter(fn($value) => in_array($value, [self::TYPE_TEXT->value, self::TYPE_TEXTAREA->value, self::TYPE_DATE->value]))
+            ->values()
+            ->toArray();
+    }
 }
