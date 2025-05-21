9994175b3c0cceb88756fcd0e0e1af3f29d95d04
MoisesCorcho <mcorchoperez@gmail.com>
2025-05-20 22:09:58 -0500

    feat: memberPosition filament resource has been added

---
+++ app/Filament/Resources/MemberPositionResource/Pages/EditMemberPosition.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Filament\Resources\MemberPositionResource\Pages;
+
+use App\Filament\Resources\MemberPositionResource;
+use Filament\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditMemberPosition extends EditRecord
+{
+    protected static string $resource = MemberPositionResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\DeleteAction::make(),
+        ];
+    }
+}
