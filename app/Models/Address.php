1e239ef08962e48862ce763fec96629dbbee119c
MoisesCorcho <mcorchoperez@gmail.com>
2025-06-13 13:40:37 -0500

    feat: add fullAddress accessor to Address model for formatted address output

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Support\Str;
 
 class Address extends Model
 {
@@ -20,5 +22,25 @@ class Address extends Model
     public function addressable(): MorphTo
     {
         return $this->morphTo();
+    }
+
+    protected function fullAddress(): Attribute
+    {
+        return Attribute::make(
+            get: function (): string {
+                $city = $this->city ? Str::ucfirst($this->city) : '';
+                $state = $this->state ? Str::ucfirst($this->state) : '';
+                $country = $this->country ? Str::ucfirst($this->country) : '';
+
+                return trim(sprintf(
+                    '%s, %s, %s, %s, %s',
+                    $this->street,
+                    $city,
+                    $state,
+                    $country,
+                    $this->zip_code
+                ));
+            },
+        );
     }
 }
