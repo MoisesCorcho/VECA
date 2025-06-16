de98c9562009f1209e90f273e88ab90960352e56
MoisesCorcho <mcorchoperez@gmail.com>
2025-06-16 14:07:07 -0500

    feat: Add migration to create foreign key for survey_id in users table

---
+++ database/migrations/2025_06_16_175005_add_survey_foreign_key_to_users_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Models\Survey;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->foreignIdFor(Survey::class, 'survey_id')->nullable()->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropForeign(['survey_id']);
+            $table->dropColumn('survey_id');
+        });
+    }
+};
