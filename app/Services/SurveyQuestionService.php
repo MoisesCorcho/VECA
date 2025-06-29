219f2d1e278db53d9b4a61d2f191e05713884437
MoisesCorcho <mcorchoperez@gmail.com>
2025-06-29 18:51:21 -0500

    feat: implement dynamic options retrieval for survey questions and handle empty states

--- app/Services/SurveyQuestionService.php
+++ app/Services/SurveyQuestionService.php
@@ -4,9 +4,15 @@ declare(strict_types=1);
 
 namespace App\Services;
 
+use App\Models\Member;
+use App\Models\MemberPosition;
 use App\Models\SurveyQuestion;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Exception;
+use App\Models\Organization;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Collection as SupportCollection;
+use Illuminate\Support\Facades\Auth;
 
 final class SurveyQuestionService
 {
@@ -29,5 +35,83 @@ final class SurveyQuestionService
         if (! $parent->canHaveChildren()) {
             throw new Exception('Maximum depth level reached.');
         }
+    }
+
+
+    public static function getOptionsLabel(string $entity)
+    {
+        $entity = new $entity();
+
+        return array_combine($entity->getFillable(), $entity->getFillable());
+    }
+
+    public static function getDatabaseOptions(string $entity, SurveyQuestion $question, array $answers): Collection | SupportCollection
+    {
+        return match ($entity) {
+            Organization::class => self::getOrganizationOptions($question),
+            Member::class => self::getMemberOptions($question, $answers),
+            MemberPosition::class => self::getMemberPositionOptions($question),
+        };
+    }
+
+    private static function getOrganizationOptions(SurveyQuestion $question): Collection
+    {
+        $organizations = Organization::where('user_id', Auth::user()->id)->select('id', $question->options_label_column)->get();
+
+        if ($organizations->isEmpty()) {
+            return collect([
+                (object) [
+                    "id" => 'not_found',
+                    $question->options_label_column => __("No organizations for this user found.")
+                ]
+            ]);
+        }
+
+        return $organizations;
+    }
+
+    private static function getMemberOptions(SurveyQuestion $question, array $answers): Collection | SupportCollection
+    {
+        $organizationQuestion = $question?->survey?->questions()->where('options_model', Organization::class)->first();
+
+        if (! $organizationQuestion) {
+            return collect([
+                (object) [
+                    "id" => "not_found",
+                    $question->options_label_column => __("Configuration error: Organization question not found.")
+                ]
+            ]);
+        }
+
+        $members = Member::where('organization_id', $answers[$organizationQuestion->id] ?? null)
+            ->select('id', $question->options_label_column)
+            ->get();
+
+        if ($members->isEmpty()) {
+            return collect([
+                (object) [
+                    "id" => "not_found",
+                    $question->options_label_column => __("No members found for this organization.")
+                ]
+            ]);
+        }
+
+        return $members;
+    }
+
+    private static function getMemberPositionOptions(SurveyQuestion $question): Collection | SupportCollection
+    {
+        $memberPositions = MemberPosition::all();
+
+        if ($memberPositions->isEmpty()) {
+            return collect([
+                (object) [
+                    "id" => "not_found",
+                    $question->options_label_column => __("No members positions found.")
+                ]
+            ]);
+        }
+
+        return $memberPositions;
     }
 }
